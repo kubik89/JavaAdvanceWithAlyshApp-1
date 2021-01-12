92f0925ab4d8f2a187c53a021d666a9cf6bbf452
Volodymyr <kubik89@yahoo.com>
2021-01-12 12:55:22 +0200

    Lection26 with Injection

--- src/main/java/ua/vbodnar/springcourceWithAlys/dao/PersonDAO.java
+++ src/main/java/ua/vbodnar/springcourceWithAlys/dao/PersonDAO.java
@@ -3,44 +3,132 @@ package ua.vbodnar.springcourceWithAlys.dao;
 import org.springframework.stereotype.Component;
 import ua.vbodnar.springcourceWithAlys.models.Person;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class PersonDAO {
     private static int PEOPLE_COUNT;
-    private List<Person> people;
-    {
-        people = new ArrayList<>();
-        people.add(new Person(++PEOPLE_COUNT, "Ihor", 10, "ihor10@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Roman", 15, "roman15@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Oleh", 20, "oleh20@gmail.com"));
-        people.add(new Person(++PEOPLE_COUNT, "Illya", 26, "illya26@gmail.com"));
+
+    private static final String URL = "jdbc:mysql://localhost:3306/dbwithalys?serverTimezone=UTC";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "root";
+
+    private static Connection connection;
+
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
-    public List<Person> allPeople () {
+
+    public List<Person> allPeople() {
+        List<Person> people = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            String SQL = "SELECT * FROM Person";
+            ResultSet resultSet = statement.executeQuery(SQL);
+            while (resultSet.next()) {
+                Person person = new Person();
+// для кожного значення, яке отримую із бази, для обєкта person встановлюю його поля:
+//   - де columnlabel в дужках - це назва поля в БД
+                person.setId(resultSet.getInt("id"));
+                person.setName(resultSet.getString("name"));
+                person.setEmail(resultSet.getString("email"));
+                person.setAge(resultSet.getInt("age"));
+
+                people.add(person);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
         return people;
     }
 
-    public Person show (int id) {
-// стрімом віфільтрувати ліст people по вхідному id, якщо такий присутній, якщо ні - повернути null
-        return people.stream().filter(person -> person.getId()==id).findAny().orElse(null);
+    public Person show(int id) {
+        Person person = null;
+
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement("SELECT * FROM person where id=?");
+            preparedStatement.setInt(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+// ----------
+// вище ми створили пустий обєкт і цей код, якщо в нас декілька обєктів із однаковим id,
+// то тоді покажемо перший з них. Але якщо в БД id в автоінкременті - такого не писати
+            resultSet.next();
+            person = new Person();
+
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setEmail(resultSet.getString("email"));
+            person.setAge(resultSet.getInt("age"));
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return person;
     }
-// метод який додасть нову людину в ліст і перед додаванням встановить їй наступну ID
+
+    // метод який додасть нову людину в ліст і перед додаванням встановить їй наступну ID
     public void save(Person person) {
-        person.setId(++PEOPLE_COUNT);
-        people.add(person);
+//        person.setId(++PEOPLE_COUNT);
+//        people.add(person);
+
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                    "insert into person values(1, ?, ?, ?)");
+            preparedStatement.setString(1, person.getName());
+            preparedStatement.setInt(2, person.getAge());
+            preparedStatement.setString(3, person.getEmail());
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
     public void update(int id, Person updatedPerson) {
-        Person personToBeUpdated = show(id);
 
-        personToBeUpdated.setName(updatedPerson.getName());
-        personToBeUpdated.setAge(updatedPerson.getAge());
-        personToBeUpdated.setEmail(updatedPerson.getEmail());
+        try {
+            PreparedStatement preparedStatement =
+                    connection.prepareStatement(
+                            "UPDATE Person SET name=?, age=?, email=? where id=?");
+            preparedStatement.setString(1, updatedPerson.getName());
+            preparedStatement.setInt(2, updatedPerson.getAge());
+            preparedStatement.setString(3, updatedPerson.getEmail());
+            preparedStatement.setInt(4, id);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 
-    public void delete (int id) {
-        people.removeIf(person -> person.getId()==id);
+    public void delete(int id) {
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement("DELETE from Person where id=?");
+
+            preparedStatement.setInt(1, id);
+
+            preparedStatement.executeUpdate();
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
     }
 }
